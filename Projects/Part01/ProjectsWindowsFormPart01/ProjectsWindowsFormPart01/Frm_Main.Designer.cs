e26b73864505a1be450e5f5f223c2ce6253af622
MinhPhuc <phuc@lhu.edu.vn>
2017-12-01 18:10:45 +0700

    update by admin

---
+++ Projects/Part01/ProjectsWindowsFormPart01/ProjectsWindowsFormPart01/Frm_Main.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace ProjectsWindowsFormPart01
+{
+    partial class Frm_Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.btnchuong01 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.btnchuong01);
+            this.flowLayoutPanel1.Controls.Add(this.button1);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(1079, 291);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // btnchuong01
+            // 
+            this.btnchuong01.BackColor = System.Drawing.Color.Teal;
+            this.btnchuong01.FlatAppearance.BorderSize = 0;
+            this.btnchuong01.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnchuong01.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnchuong01.ForeColor = System.Drawing.Color.White;
+            this.btnchuong01.Location = new System.Drawing.Point(3, 3);
+            this.btnchuong01.Name = "btnchuong01";
+            this.btnchuong01.Size = new System.Drawing.Size(219, 45);
+            this.btnchuong01.TabIndex = 0;
+            this.btnchuong01.Text = "Chương 01";
+            this.btnchuong01.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Teal;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(228, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(219, 45);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Chương 02";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // Frm_Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 23F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1079, 291);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Font = new System.Drawing.Font("Tahoma", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Name = "Frm_Main";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button btnchuong01;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
