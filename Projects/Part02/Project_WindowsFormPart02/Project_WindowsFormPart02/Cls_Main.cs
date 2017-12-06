d4daf27e37ca5d7d20b4dbb804286450e59ae80a
MinhPhuc <phuc@lhu.edu.vn>
2017-12-06 11:18:52 +0700

    update ngày 06-12-2017

---
+++ Projects/Part02/Project_WindowsFormPart02/Project_WindowsFormPart02/Cls_Main.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project_WindowsFormPart02
+{
+   public class Cls_Main
+    {
+       public static string pathconn = Application.StartupPath + @"\connection.ini";
+    }
+}
